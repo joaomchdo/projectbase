ebd7379ce5f775529af86f74231c5c04797f2b05
joaomchdo <joaoclevertonmachado@gmail.com>
2025-05-20 16:50:34 -0300

    variaveis, comentario, alert e consoles

---
+++ logicaconjs.js
@@ -0,0 +1,25 @@
+/*let bruna = 6.5;
+let carla = 8;
+let soma = bruna+carla
+console.log(" A soma é: " + soma)
+console.log("-------Proxima questão-----------")
+let thiago = 7;
+let lara = 5;
+let resultado = thiago*lara
+console.log(" A mutiplicação de thiago e a de lara é igual á:" + resultado )
+console.log("-------Próxima questão------------")
+let otavio = 4;
+let leticia = 5;
+let valor = otavio/leticia
+console.log(" A divisão de otavio e de leticia é: " + valor )
+console.log("-------Proxima questão--------")
+let harry = 8 
+let hermione = 7
+let final = harry-hermione
+console.log("A subtração de harry e de hermione é: " + final)*/
+console.log("-------------Questão N°2-------------------------")
+let nome_do_usuario="Joao"
+let salario = 4300
+let meses_trab = 9 
+let decimo = salario/12*meses_trab
+console.log(" nome_do_usuario " + " Seu salario é: " + decimo +  "PARABÉNS!!")
