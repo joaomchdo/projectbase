ca6f6c8d4422a0443dca77e5c6efe5bebd677030
joaomchdo <joaoclevertonmachado@gmail.com>
2025-05-22 17:07:28 -0300

    Aprendendo Promt

--- logicaconjs.js
+++ logicaconjs.js
@@ -17,9 +17,11 @@ let harry = 8
 let hermione = 7
 let final = harry-hermione
 console.log("A subtração de harry e de hermione é: " + final)*/
-console.log("-------------Questão N°2-------------------------")
+/*console.log("-------------Questão N°2-------------------------")
 let nome_do_usuario="Joao"
 let salario = 4300
 let meses_trab = 9 
 let decimo = salario/12*meses_trab
-console.log(" nome_do_usuario " + " Seu salario é: " + decimo +  "PARABÉNS!!")
+ console.log(" nome_do_usuario: " + " Seu salario é: " + decimo +  "PARABÉNS!!")*/
+
+ let x = Number(prompt("Digite um número"))
