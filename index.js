664a54cac336ed6afa25e8f778c7263e5872da49
joaomchdo <joaoclevertonmachado@gmail.com>
2025-06-12 17:02:21 -0300

    inicializandodom

---
+++ index.js
@@ -0,0 +1,7 @@
+/*let leitura = document.getElementsByTagName("li")[3]
+leitura.style.color = "gray"
+let leitura = document.getElementsByName("h4dadiv")[0]
+leitura.innerText = "Ja fui e voltei !!!"
+leitura.style.color = "red"*/
+let leitura = document.getElementById("atalaia")
+leitura.style.background = "gray"
